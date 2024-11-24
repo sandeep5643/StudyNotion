5080ca727eb4e2bb968ca66377bd1119a1d9b45e
sandeep kuamr <sandeekumar80832718@gmail.com>
2024-11-24 17:25:11 +0530

    changes in razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,7 +1,16 @@
-const Razorpay = require('razorpay');
-require('dotenv').config();
+// const Razorpay = require('razorpay');
+// require('dotenv').config();
 
-exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    key_secret: process.env.RAZORPAY_SECRET
+// exports.instance = new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY,
+//     key_secret: process.env.RAZORPAY_SECRET
+// });
+
+const Razorpay = require('razorpay');
+
+const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,    // Make sure the key is correctly set from environment variables
+    key_secret: process.env.RAZORPAY_SECRET,  // Same for the secret key
 });
+
+module.exports = razorpay;
